c5c64f737947ff81772009f86b8eb6efaf990db6
ENYE APPS <eule.weajinelette@gmail.com>
2023-07-04 13:47:49 +0800

    CATALOGS unfinished

---
+++ lib/screens/catalogs/catalogs.dart
@@ -0,0 +1,32 @@
+//values na meron si sub categories
+class Catalogs {
+  String id;
+  String model_name;
+  String sized;
+  String sale_price;
+  String image;
+  String products_id;
+  String manufacturer_id;
+
+  Catalogs({
+    required this.id,
+    required this.model_name,
+    required this.sized,
+    required this.sale_price,
+    required this.image,
+    required this.products_id,
+    required this.manufacturer_id,
+  });
+
+  factory Catalogs.fromJson(Map<String, dynamic> json) {
+    return Catalogs(
+      id: json['subProd_id'] as String,
+      model_name: json['model_name'] as String,
+      sized: json['sized'] as String,
+      sale_price: json['sale_price'] as String,
+      image: json['image'] as String,
+      products_id: json['products_id'] as String,
+      manufacturer_id: json['manufacturer_id'] as String,
+    );
+  }
+}
