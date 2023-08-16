744a891f112bf11c6c5a9b9e75978019deb6a269
weyawei <eule.weajinelette@gmail.com>
2023-08-16 10:53:56 +0800

    linis linis

--- lib/screens/technical_schedule/tasktile_page.dart
+++ lib/screens/technical_schedule/tasktile_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 
@@ -12,16 +11,15 @@ class TaskTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding:
-      EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.only(bottom: 12),
+      margin: const EdgeInsets.only(bottom: 12),
       child: Container(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         //  width: SizeConfig.screenWidth * 0.78,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
-          color: _getStatusColor(services?.status),
+          color: _getStatusColor(services.status),
         ),
         child: Row(children: [
           Expanded(
@@ -29,7 +27,7 @@ class TaskTile extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  "#${services?.svcId}",
+                  "#${services.svcId}",
                   style: GoogleFonts.lato(
                     textStyle: const TextStyle(
                       fontSize: 16,
@@ -41,30 +39,26 @@ class TaskTile extends StatelessWidget {
                 ),
                 const SizedBox(height: 12,),
                 Text(
-                  services?.svcTitle??"",
+                  services.svcTitle,
                   style: GoogleFonts.lato(
-                    textStyle: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
+                    textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
                   ),
                 ),
-                SizedBox(
-                  height: 12,
-                ),
+                const SizedBox(height: 12,),
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-
-                    SizedBox(width: 20),
                     Icon(
                       Icons.calendar_month_rounded,
                       color: Colors.grey[200],
                       size: 18,
                     ),
-                    SizedBox(width: 4),
+                    const SizedBox(width: 4),
                     Text(
-                      DateFormat.yMMMd().format(DateTime.parse(services!.dateSched)),
+                      DateFormat.yMMMd().format(DateTime.parse(services.dateSched)),
                       style: GoogleFonts.lato(
                         textStyle:
                         TextStyle(fontSize: 15, color: Colors.grey[100]),
@@ -72,24 +66,24 @@ class TaskTile extends StatelessWidget {
                     ),
                   ],
                 ),
-                SizedBox(height: 12),
+                const SizedBox(height: 12),
                 RichText(
                   softWrap: true,
                   text: TextSpan(children: <TextSpan>
                   [
-                    TextSpan(text: "${services!.clientName} || ",
+                    TextSpan(text: "${services.clientName} || ",
                       style: GoogleFonts.lato(
                         textStyle: TextStyle(fontSize: 14, color: Colors.grey[100], letterSpacing: 0.8),
                       ),),
-                    TextSpan(text: "${services!.clientCompany} || ",
+                    TextSpan(text: "${services.clientCompany} || ",
                       style: GoogleFonts.lato(
                         textStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.grey[100], letterSpacing: 0.8),
                       ),),
-                    TextSpan(text: "${services!.clientContact} || ",
+                    TextSpan(text: "${services.clientContact} || ",
                       style: GoogleFonts.lato(
                         textStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.grey[100], letterSpacing: 0.8),
                       ),),
-                    TextSpan(text: "${services!.clientContact}",
+                    TextSpan(text: services.clientContact,
                       style: GoogleFonts.lato(
                         textStyle: TextStyle(fontSize: 14, color: Colors.grey[100], letterSpacing: 0.8),
                       ),),
@@ -100,7 +94,7 @@ class TaskTile extends StatelessWidget {
             ),
           ),
           Container(
-            margin: EdgeInsets.symmetric(horizontal: 10),
+            margin: const EdgeInsets.symmetric(horizontal: 10),
             height: 60,
             width: 0.5,
             color: Colors.grey[200]!.withOpacity(0.7),
@@ -108,9 +102,9 @@ class TaskTile extends StatelessWidget {
           RotatedBox(
             quarterTurns: 3,
             child: Text(
-              services!.status.toUpperCase(),
+              services.status.toUpperCase(),
               style: GoogleFonts.lato(
-                textStyle: TextStyle(
+                textStyle: const TextStyle(
                     fontSize: 12,
                     fontWeight: FontWeight.bold,
                     color: Colors.white),
