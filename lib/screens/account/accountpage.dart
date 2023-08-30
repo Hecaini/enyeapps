b898aa32392e671c75f671604b807cfde72877ea
weyawei <eule.weajinelette@gmail.com>
2023-08-30 09:42:10 +0800

    change yung Complete Page display

--- lib/screens/account/accountpage.dart
+++ lib/screens/account/accountpage.dart
@@ -160,7 +160,7 @@ class _AccountPageState extends State<AccountPage> {
                     NormalTextField(
                       controller: positionController,
                       hintText: 'Position',
-                      disabling: disabling,
+                      disabling: true,
                     ),
                   ],
                 ),
